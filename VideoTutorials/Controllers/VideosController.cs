2f7d830869af968e6b252beebcdf777f7a157325
ivanastojkovska <i.stojkovska@outlook.com>
2015-06-16 00:25:14 +0200

    Version 8 - Team Project

---
+++ VideoTutorials/Controllers/VideosController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VideoTutorials.Models;
+
+namespace VideoTutorials.Controllers
+{
+    [Authorize]
+    public class VideosController : Controller
+    {
+        private VideoTutorialsDbContext db = new VideoTutorialsDbContext();
+
+        // GET: /Videos/
+        [Authorize(Roles="admin")]
+        public ActionResult Index()
+        {
+            var videos = db.Videos.Include(v => v.Categories);
+            return View(videos.ToList());
+        }
+
+        // GET: /Videos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Video video = db.Videos.Find(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+
+        // GET: /Videos/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "Name");
+            return View();
+        }
+
+        // POST: /Videos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="VideoID,Title,Description,Link,Tags,Thumbnail,CategoryID")] Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Videos.Add(video);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "Name", video.CategoryID);
+            return View(video);
+        }
+
+        // GET: /Videos/Edit/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Video video = db.Videos.Find(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "Name", video.CategoryID);
+            return View(video);
+        }
+
+        // POST: /Videos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "admin")]
+        public ActionResult Edit([Bind(Include="VideoID,Title,Description,Link,Tags,Thumbnail,CategoryID")] Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(video).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "Name", video.CategoryID);
+            return View(video);
+        }
+
+        // GET: /Videos/Delete/5
+        [Authorize(Roles = "admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Video video = db.Videos.Find(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+
+        // POST: /Videos/Delete/5
+        [Authorize(Roles = "admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Video video = db.Videos.Find(id);
+            db.Videos.Remove(video);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult VideoList()
+        {
+            var videos = db.Videos.Include(v => v.Categories);
+            return View(videos.ToList());
+        }
+        public ActionResult WatchVideo(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Video video = db.Videos.Find(id);
+            if (video == null)
+            {
+                return HttpNotFound();
+            }
+            return View(video);
+        }
+    }
+}
---
+++ VideoTutorials/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VideoTutorials.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public DateTime DateRegistered { get; set; }
+        public string Roles { get; set; }
+        public User()
+        {
+            this.Roles = "";
+        }
+        public virtual ICollection<Video> Videos { get; set; }
+    }
+}
